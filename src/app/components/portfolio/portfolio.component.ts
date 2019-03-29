a160c70afedfd91d76c809752b273c3b6134de56
Fares Stita <stita.fares@gmail.com>
2019-03-29 19:10:54 +0100

    6.5

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { PortfolioService } from '../../services/portfolio.service';
 import { Portfolio } from '../../services/portfolio.model';
+import { trigger, transition, animate, style, stagger, animateChild, query } from '@angular/animations';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
+  animations: [
+    trigger('list', [
+      transition(':enter', [
+        query('@items', stagger(60, animateChild()))
+      ])
+    ]),
+    trigger('items', [
+      transition(':enter', [
+        style({ transform: 'scale(0)', opacity: 0 }),
+        animate('0.350s cubic-bezier(.8,-0.6,0.2,1.5)',
+          style({ transform: 'scale(1)', opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class PortfolioComponent implements OnInit {
 
