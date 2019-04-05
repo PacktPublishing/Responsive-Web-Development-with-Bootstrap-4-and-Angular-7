e5dae4054d7b6523bb2b282572b718413e79a5bb
Fares Stita <stita.fares@gmail.com>
2019-04-05 14:08:21 +0200

    7.2

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
-import { MatCardModule, MatDividerModule, MatProgressBarModule, MatButtonModule, MatRadioModule } from '@angular/material';
+import {
+  MatCardModule, MatDividerModule, MatProgressBarModule, MatButtonModule, MatRadioModule,
+  MatFormFieldModule, MatInputModule
+} from '@angular/material';
 
 @NgModule({
-  imports: [MatCardModule, MatDividerModule, MatProgressBarModule, MatButtonModule, MatRadioModule],
-  exports: [MatCardModule, MatDividerModule, MatProgressBarModule, MatButtonModule, MatRadioModule],
+  imports: [MatCardModule, MatDividerModule, MatProgressBarModule, MatButtonModule, MatRadioModule,
+    MatFormFieldModule, MatInputModule],
+  exports: [MatCardModule, MatDividerModule, MatProgressBarModule, MatButtonModule, MatRadioModule,
+    MatFormFieldModule, MatInputModule],
 })
 export class MaterialModule { }
